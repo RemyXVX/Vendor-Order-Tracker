3ed2899292f850ef5f35df04690fde4d7534c18c
RemyXVX <remyfranciscoflores@gmail.com>
2022-12-17 23:57:10 -0800

    Test 1 in Vendor, created and vendor order list

--- VendorOrder.Tests/ModelsTests/Vendor.Tests.cs
+++ VendorOrder.Tests/ModelsTests/Vendor.Tests.cs
@@ -6,8 +6,19 @@ using VendorOrder.Models;
 namespace VendorOrder.Tests
 {
   [TestClass]
-  public class VendorTest
+  public class VendorTest : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreateVendorOrder_Vendor()
+    {
+      Vendor newVendor = new Vendor("Bob the Builder", "He builds things", "A stack of wood");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
 
   }
 }
--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace VendorOrder.Models
 {
   public class Vendor
@@ -5,7 +7,24 @@ namespace VendorOrder.Models
     //Name
     //Description
     //Orders
-    //
-    //
+
+    public static List<Vendor> _instances = new List<Vendor> {};
+
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public string Order {get; set;}
+    public List<Order> Orders {get; set;}
+
+    public Vendor (string name, string description, string order)
+    {
+      Name = name;
+      Description = description;
+      Order = order;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
