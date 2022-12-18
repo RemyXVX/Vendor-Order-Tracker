ffa3aac5bffe7d171f398d8ce11ef2956137ff58
RemyXVX <remyfranciscoflores@gmail.com>
2022-12-18 05:02:08 -0800

    Updated Program file

--- VendorOrder/Program.cs
+++ VendorOrder/Program.cs
@@ -1,12 +1,29 @@
 using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace VendorOrder
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      WebApplication app = builder.Build();
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+  
+      app.Run();
     }
   }
 }
