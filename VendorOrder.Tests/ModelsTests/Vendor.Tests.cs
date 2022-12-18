fe5b81d983f448bbe07e08681f352b773d9c7e04
RemyXVX <remyfranciscoflores@gmail.com>
2022-12-18 00:58:56 -0800

    Test 4, created a vendor list

--- VendorOrder.Tests/ModelsTests/Vendor.Tests.cs
+++ VendorOrder.Tests/ModelsTests/Vendor.Tests.cs
@@ -41,20 +41,20 @@ namespace VendorOrder.Tests
     [TestMethod]
     public void GetOrder_ReturnOrder_String()
     {
-      string order = "He build things";
+      string order = "A stack of wood";
       Vendor newVendor = new Vendor("Bob the Builder", "He builds things", order);
       string result = newVendor.Order;
       Assert.AreEqual(order, result);
     }
 
-    // [TestMethod]
-    // public void GetDescription_ReturnDescription_String()
-    // {
-    //   string description = "He build things";
-    //   Vendor newVendor = new Vendor("Bob the Builder", description, "A stack of wood");
-    //   string result = newVendor.Description;
-    //   Assert.AreEqual(description, result);
-    // }
+    [TestMethod]
+    public void GetAllVendors_ReturnAllVendors_List()
+    {
+      Vendor newVendor = new Vendor("Bob the Builder", "He builds things", "A stack of wood");
+      Vendor newNewVendor = new Vendor("Thomas the train", "He goes vroom vroom", "A stack of coal");
+      List<Vendor> vendorList = new List<Vendor> {newVendor, newNewVendor};
+      CollectionAssert.AreEqual(vendorList, Vendor.GetAll());
+    }
 
   }
 }
